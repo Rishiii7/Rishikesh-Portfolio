1d1ce666fa07645019e397136da12eb54598b5b8
= <7rishi20ss@gmail.com>
2024-06-15 23:33:38 -0600

    added custom button

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -37,7 +37,7 @@ const Projects = () => {
                     <p className='mb-4 text-lg text-neutral-400'>{project.description}</p>
                     <div className='flex flex-wrap justify-center items-center space y-8'>
                         {project.technologies.map((tech, index) => (
-                            <span key={index} className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm sm:text-base md:text-lg font-bold text-purple-900'>
+                            <span key={index} className='mr-2 text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>
                                 {tech}
                             </span>
                         ))}

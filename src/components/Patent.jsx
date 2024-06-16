e4226254338812b96ab4936d274dcac9fb251201
= <7rishi20ss@gmail.com>
2024-06-15 23:33:17 -0600

    added custom button

--- src/components/Patent.jsx
+++ src/components/Patent.jsx
@@ -10,7 +10,7 @@ const Patent = () => {
         whileInView={{opacity: 1, y: 0}}
         initial={{opacity: 0, y: -100}}
         transition={{duration: 0.5}}
-        className='my-20 text-center text-5xl'>Patent</motion.h2>
+        className='my-20 text-center text-5xl'>Research Paper</motion.h2>
         <div>
             {PATENT.map((patent, index) => (
                 <div key={index} className='mb-8 text-lg flex flex-wrap lg:justify-center'>
@@ -38,7 +38,7 @@ const Patent = () => {
                         <p className='mb-4 text-neutral-400'>{patent.description}</p>
                         <div className='flex flex-wrap justify-center items-center space y-8'>
                             {patent.technologies.map((tech, index) => (
-                                <span key={index} className='mr-1 rounded bg-neutral-900 px-2 py-1 text-sm sm:text-base md:text-lg font-bold text-purple-800'>{tech}</span>
+                                <span key={index} className='mr-1 text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>{tech}</span>
                             ))}
                         </div>
                     </motion.div>
